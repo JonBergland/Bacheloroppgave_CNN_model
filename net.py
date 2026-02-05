d19f7dfdeb0b3a3502403e4cec248a0e86bd3824
Jon Bergland <jobergl@stud.ntnu.no>
2026-02-05 11:52:07 +0100

    feat: add more layers to the neural net

--- net.py
+++ net.py
@@ -5,31 +5,51 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, num_classes: int):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
+
+        # 1 layer
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        # 2 layer
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        # 3 layer
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        # 4 layer
+        self.conv4 = nn.Conv2d(128, 256, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.dropout = nn.Dropout(0.5)
 
-        self.fc1 = nn.Linear(1, 120)
-
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
-
-        self._init_fc()
+        self._init_fc(num_classes=num_classes)
 
 
-    def _init_fc(self):
+    def _init_fc(self, num_classes):
         with torch.no_grad():
             x = torch.zeros(1, 1, 32, 32)
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
+            x = self.pool(F.relu(self.bn1(self.conv1(x))))
+            x = self.pool(F.relu(self.bn2(self.conv2(x))))
+            x = self.pool(F.relu(self.bn3(self.conv3(x))))
+            x = self.pool(F.relu(self.bn4(self.conv4(x))))
+
             n_features = x.numel()
-            self.fc1 = nn.Linear(n_features, 120)
+
+        self.fc1 = nn.Linear(n_features, 512)
+        self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+
         x = x.view(x.size(0), -1)
+        
+        x = self.dropout(F.relu(self.fc1(x)))
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
