2d7b511b7756647dd5ab23479a7d8ceb3d0c6157
Jon Bergland <jobergl@stud.ntnu.no>
2026-02-09 09:32:29 +0100

    feat: add residual-functionality to the neural net

--- net.py
+++ net.py
@@ -2,53 +2,72 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(channels, channels, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
+
+        self.conv2 = nn.Conv2d(channels, channels, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        identity = x
+
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        out += identity
+        out = F.relu(out)
+
+        return out
+
+
+
 class Net(nn.Module):
     def __init__(self, num_classes: int, img_size: int):
         super(Net, self).__init__()
 
-        # 1 layer
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
 
-        # 2 layer
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.block1 = ResidualBlock(32)
+        self.block2 = ResidualBlock(32)
+        self.block3 = ResidualBlock(32)
+
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
 
-        # 3 layer
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
+        self.block4 = ResidualBlock(64)
+        self.block5 = ResidualBlock(64)
+        self.block6 = ResidualBlock(64)
 
-        # 4 layer
-        self.conv4 = nn.Conv2d(128, 256, 3, padding=1)
-        self.bn4 = nn.BatchNorm2d(256)
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
 
-        self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.5)
-
-        self._init_fc(num_classes=num_classes, img_size=img_size)
-
-
-    def _init_fc(self, num_classes, img_size):
-        with torch.no_grad():
-            x = torch.zeros(1, 1, img_size, img_size)
-            x = self.pool(F.relu(self.bn1(self.conv1(x))))
-            x = self.pool(F.relu(self.bn2(self.conv2(x))))
-            x = self.pool(F.relu(self.bn3(self.conv3(x))))
-            x = self.pool(F.relu(self.bn4(self.conv4(x))))
-
-            n_features = x.numel()
-
-        self.fc1 = nn.Linear(n_features, 512)
-        self.fc2 = nn.Linear(512, num_classes)
+        self.fc1 = nn.Linear(64, 256)
+        # self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
-        x = self.pool(F.relu(self.bn3(self.conv3(x))))
-        x = self.pool(F.relu(self.bn4(self.conv4(x))))
 
+        x = F.relu(self.bn1(self.conv1(x)))
+
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+
+        x = F.relu(self.bn2(self.conv2(x)))
+
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+
+        x = self.pool(x)
         x = x.view(x.size(0), -1)
-        
-        x = self.dropout(F.relu(self.fc1(x)))
+
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
+
         return x
