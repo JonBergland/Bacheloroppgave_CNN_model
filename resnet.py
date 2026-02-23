5834304b7ec35d27236b09ab78c77d02ca072332
Jon Bergland <jobergl@stud.ntnu.no>
2026-02-23 11:37:20 +0100

    feat: add class Resnet9

--- resnet.py
+++ resnet.py
@@ -47,6 +47,7 @@ class ResNet18(nn.Module):
         self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
         
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(512, num_classes)
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
@@ -69,5 +70,47 @@ class ResNet18(nn.Module):
         
         out = self.avgpool(out)
         out = out.view(out.size(0), -1)
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
+
+class ResNet9(nn.Module):
+    def __init__(self, num_classes=10, in_channels=1):
+        super(ResNet9, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.in_channels = 64
+        
+        self.layer1 = self._make_layer(BasicBlock, 64, 1, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 1, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 1, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 1, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.dropout(out)
         out = self.fc(out)
         return out
