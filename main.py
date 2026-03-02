9d2e7b304096801d298ffd9cfc607013a5d908bd
Jon Bergland <jobergl@stud.ntnu.no>
2026-03-02 09:11:19 +0100

    refactor: change image size from 32 to 128

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main(dataset_root: str,
          lr_rate: float = 0.01,
          momentum: float = 0.09,
          batch_size: int = 32,
-         img_size: int = 32, 
+         img_size: int = 128, 
          manual_seed: int = 42,
          save_path: str | None = None,
          only_see_metrics: bool = False):
@@ -36,13 +36,13 @@ if __name__ == '__main__':
     root = os.path.join(BASE_DIR, "dataset")
     save_path = os.path.join(BASE_DIR, "saved_models")
 
-    epochs = 2
+    epochs = 0
     lr_rate = 0.001
     momentum = 0.9
     batch_size = 32
-    img_size = 32
+    img_size = 128
     manual_seed = 42
-    only_see_metrics = False
+    only_see_metrics = True
     
 
 
