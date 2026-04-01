e02bb04209582a317a6c76a8f13fb577e7200a64
Jon Bergland <jobergl@stud.ntnu.no>
2026-04-01 19:16:56 +0200

    refactor: move declaration of baseTransforms to method getBaseTransforms

--- data_processing/data_augmentation.py
+++ data_processing/data_augmentation.py
@@ -10,15 +10,13 @@ class DataAugmentation:
         self.img_size = img_size
         self.output_channels = output_channels
 
-        self.base_transforms = [
-            v2.Resize((self.img_size, self.img_size)),
-            v2.Grayscale(num_output_channels=self.output_channels),
-            v2.ToTensor(),
-            v2.Normalize(mean=[0.449], std=[0.226]),
-        ]
-
     def getBaseTransform(self):
-        return self.base_transforms
+        return [
+        v2.Resize((self.img_size, self.img_size)),
+        v2.Grayscale(num_output_channels=self.output_channels),
+        v2.ToTensor(),
+        v2.Normalize(mean=[0.449], std=[0.226]),
+    ]
 
     def getDataAugmentations(
         self,
@@ -33,17 +31,17 @@ class DataAugmentation:
         transforms = []
 
         if horizontal_flip:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t.insert(1, v2.RandomHorizontalFlip(p=1.0))
             transforms.append(v2.Compose(t))
 
         if vertical_flip:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t.insert(1, v2.RandomVerticalFlip(p=1.0))
             transforms.append(v2.Compose(t))
 
         if resizing:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t[0] = v2.RandomResizedCrop(
                 size=(self.img_size, self.img_size),
                 scale=(0.8, 1.0),
@@ -52,17 +50,17 @@ class DataAugmentation:
             transforms.append(v2.Compose(t))
 
         if translation:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t.insert(1, v2.RandomAffine(degrees=0, translate=(0.1, 0.1)))
             transforms.append(v2.Compose(t))
 
         if blur:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t.insert(1, v2.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0)))
             transforms.append(v2.Compose(t))
 
         if color_jitter:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t.insert(
                 1,
                 v2.ColorJitter(
@@ -75,7 +73,7 @@ class DataAugmentation:
             transforms.append(v2.Compose(t))
 
         if random_erasing:
-            t = self.base_transforms.copy()
+            t = self.getBaseTransform()
             t.append(v2.RandomErasing(p=1.0, scale=(0.02, 0.2), ratio=(0.3, 3.3)))
             transforms.append(v2.Compose(t))
 
